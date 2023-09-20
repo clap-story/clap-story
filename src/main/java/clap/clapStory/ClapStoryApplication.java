36501fcf66cc86ecd9d770f9fade95d2b99b0557
SoohoPark <91055103+clapho@users.noreply.github.com>
2023-09-21 04:14:51 +0900

    [KAN-3] 프로젝트를 빌드한다. (#1)
    * chore: 프로젝트 생성
    
    * fix: git 캐시 제거

---
+++ src/main/java/clap/clapStory/ClapStoryApplication.java
@@ -0,0 +1,13 @@
+package clap.clapStory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClapStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClapStoryApplication.class, args);
+	}
+
+}
