3d76fefc1194bfeb812862f68ffee914ca1b4e43
SoohoPark <clapp301@gmail.com>
2023-09-26 23:58:11 +0900

    refactor: Column name 변경

--- src/main/java/clap/clapStory/domain/Post.java
+++ src/main/java/clap/clapStory/domain/Post.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,18 +11,17 @@ import org.springframework.data.annotation.LastModifiedDate;
 import java.time.LocalDateTime;
 
 @Entity(name = "posts")
-@Table
 @Getter
 public class Post {
 
     @Id @GeneratedValue
-    @Column(name = "post_id")
+    @Column(name = "id")
     private Long id;
 
-    @Column(name = "post_title")
+    @Column(name = "title")
     private String title;
 
-    @Column(name = "post_content")
+    @Column(name = "content")
     private String content;
 
     @CreatedDate
